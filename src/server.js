ee94d5c36dedc0e136f90a0ecaa1a85f693894b5
Kusochek-humora <69989303+Kusochek-humora@users.noreply.github.com>
2026-05-25 18:29:43 +0500

    docker start

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import Fastify from 'fastify'
+
+const app = Fastify({ logger: true })
+
+app.get('/', async () => {
+  return { message: 'Hello World' }
+})
+
+app.listen({ port: 3000, host: '0.0.0.0' }, (err) => {
+  if (err) {
+    app.log.error(err)
+    process.exit(1)
+  }
+})
