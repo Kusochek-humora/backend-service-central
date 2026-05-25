2f9c45bb8272b3a51f2be81fbbfd1b84b9d44bea
Kusochek-humora <69989303+Kusochek-humora@users.noreply.github.com>
2026-05-25 20:36:24 +0500

    fix and new script server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,26 @@
-import Fastify from 'fastify'
+import fastify from "fastify";
+import fastifyJwt from "@fastify/jwt";
+import "dotenv/config";
+import "reflect-metadata";
 
-const app = Fastify({ logger: true })
-
-app.get('/', async () => {
-  return { message: 'yan pedik' }
-})
-
-app.listen({ port: 3000, host: '0.0.0.0' }, (err) => {
-  if (err) {
-    app.log.error(err)
-    process.exit(1)
+const app = fastify({ logger: true });
+const start = async () => {
+  try {
+    await app.listen({ port: 3000, host: "0.0.0.0" });
+    app.log.info("server dolboeb");
+  } catch (error) {
+    app.log.error(error);
+    process.exit(1);
   }
-})
+};
+start();
+// app.get("/", async () => {
+//   return { message: "yan pedik" };
+// });
+
+// app.listen({ port: 3000, host: "0.0.0.0" }, (err) => {
+//   if (err) {
+//     app.log.error(err);
+//     process.exit(1);
+//   }
+// });
