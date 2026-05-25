16f7dc15db31527fcfef5e928341e58061cd8e76
Kusochek-humora <69989303+Kusochek-humora@users.noreply.github.com>
2026-05-25 20:20:53 +0500

    check server js (autodeploy)

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import Fastify from 'fastify'
 const app = Fastify({ logger: true })
 
 app.get('/', async () => {
-  return { message: 'Hello World' }
+  return { message: 'yan pedik' }
 })
 
 app.listen({ port: 3000, host: '0.0.0.0' }, (err) => {
